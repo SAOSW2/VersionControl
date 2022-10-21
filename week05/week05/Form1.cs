684e2a5afdd04a211a62d7fc875ede590851b7d8
SAOSW2 <uabel12@gmail.com>
2022-10-21 13:06:07 +0200

    7

--- week05/week05/Form1.cs
+++ week05/week05/Form1.cs
@@ -7,12 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using week05.MnbServiceReference;
 
 namespace week05
 {
     public partial class Form1 : Form
     {
+
+        var Rates = new BindingList<>;
+        var xml = new XmlDocument();
+        xml.LoadXml(result);
+
         public Form1()
         {
             InitializeComponent();
@@ -31,5 +37,37 @@ namespace week05
         var response = mnbService.GetExchangeRates(request);
 
         var result = response.GetExchangeRatesResult;
-    }
+
+        foreach (XmlElement element in xml.DocumentElement)
+            {
+            
+            var rate = new RateData();
+        Rates.Add(rate);
+             rate.Date = DateTime.Parse(element.GetAttribute("date"));
+             var childElement = (XmlElement)element.ChildNodes[0];
+        rate.Currency = childElement.GetAttribute("curr");
+            var unit = decimal.Parse(childElement.GetAttribute("unit"));
+        var value = decimal.Parse(childElement.InnerText);
+    if (unit != 0)
+        rate.Value = value / unit;
+
+  
+            }
+
+    chartRateData.DataSource = Rates;
+
+var series = chartRateData.Series[0];
+    series.ChartType = SeriesChartType.Line;
+series.XValueMember = "Date";
+series.YValueMembers = "Value";
+series.BorderWidth = 2;
+
+var legend = chartRateData.Legends[0];
+    legend.Enabled = false;
+
+var chartArea = chartRateData.ChartAreas[0];
+    chartArea.AxisX.MajorGrid.Enabled = false;
+chartArea.AxisY.MajorGrid.Enabled = false;
+chartArea.AxisY.IsStartedFromZero = false;
 }
+
