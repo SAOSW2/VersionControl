5c1c74dfdfdc825447f5d77cb4ea7a4ef2d94979
SAOSW2 <uabel12@gmail.com>
2022-11-11 12:43:01 +0100

    5

--- w7/Form1.cs
+++ w7/Form1.cs
@@ -7,12 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using w7.entities;
 
 namespace w7
 {
     public partial class Form1 : Form
     {
-
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
 
@@ -21,6 +22,16 @@ namespace w7
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
